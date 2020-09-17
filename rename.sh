55b616df0c6da5686d546c8de1fca6205fad8f88
Carlos Ramirez <carlware.sys@gmail.com>
2020-09-17 17:52:24 -0500

    fixed renamer

--- rename.sh
+++ rename.sh
@@ -3,18 +3,18 @@
 VENDOR=${1}
 PACKAGE=${2}
 
-find ./ -type f -iname "*.go" -exec sed -i '.bak' 's/arquil/${VENDOR}/g' {} \;
-find ./ -type f -iname "*.yml" -exec sed -i '.bak' 's/arquil/${VENDOR}/g' {} \;
-find ./ -type f -iname "*.graphqls" -exec sed -i '.bak' 's/arquil/${VENDOR}/g' {} \;
-find ./ -type f -iname "*.mod" -exec sed -i '.bak' 's/arquil/${VENDOR}/g' {} \;
-find ./ -type f -iname "*Dockerfile*" -exec sed -i '.bak' 's/arquil/${VENDOR}/g' {} \;
-find ./ -type f -iname "*Makefile*" -exec sed -i '.bak' 's/arquil/${VENDOR}/g' {} \;
+find ./ -type f -iname "*.go" -exec sed -i '.bak' 's/arquil/'"$VENDOR"'/g' {} \;
+find ./ -type f -iname "*.yml" -exec sed -i '.bak' 's/arquil/'"$VENDOR"'/g' {} \;
+find ./ -type f -iname "*.graphqls" -exec sed -i '.bak' 's/arquil/'"$VENDOR"'/g' {} \;
+find ./ -type f -iname "*.mod" -exec sed -i '.bak' 's/arquil/'"$VENDOR"'/g' {} \;
+find ./ -type f -iname "*Dockerfile*" -exec sed -i '.bak' 's/arquil/'"$VENDOR"'/g' {} \;
+find ./ -type f -iname "*Makefile*" -exec sed -i '.bak' 's/arquil/'"$VENDOR"'/g' {} \;
 find . -name '*.bak' -exec rm -rf {} +
 
-find ./ -type f -iname "*.go" -exec sed -i '.bak' 's/accounts/${PACKAGE}/g' {} \;
-find ./ -type f -iname "*.yml" -exec sed -i '.bak' 's/accounts/${PACKAGE}/g' {} \;
-find ./ -type f -iname "*.graphqls" -exec sed -i '.bak' 's/accounts/${PACKAGE}/g' {} \;
-find ./ -type f -iname "*.mod" -exec sed -i '.bak' 's/accounts/${PACKAGE}/g' {} \;
-find ./ -type f -iname "*Dockerfile*" -exec sed -i '.bak' 's/accounts/${PACKAGE}/g' {} \;
-find ./ -type f -iname "*Makefile*" -exec sed -i '.bak' 's/accounts/${PACKAGE}/g' {} \;
+find ./ -type f -iname "*.go" -exec sed -i '.bak' 's/accounts/'"$PACKAGE"'/g' {} \;
+find ./ -type f -iname "*.yml" -exec sed -i '.bak' 's/accounts/'"$PACKAGE"'/g' {} \;
+find ./ -type f -iname "*.graphqls" -exec sed -i '.bak' 's/accounts/'"$PACKAGE"'/g' {} \;
+find ./ -type f -iname "*.mod" -exec sed -i '.bak' 's/accounts/'"$PACKAGE"'/g' {} \;
+find ./ -type f -iname "*Dockerfile*" -exec sed -i '.bak' 's/accounts/'"$PACKAGE"'/g' {} \;
+find ./ -type f -iname "*Makefile*" -exec sed -i '.bak' 's/accounts/'"$PACKAGE"'/g' {} \;
 find . -name '*.bak' -exec rm -rf {} +
